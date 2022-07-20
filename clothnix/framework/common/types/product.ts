b4cd19bc7b1e59ab8b43f16bb8918043a6d3cf76
TPGitz <103758766+TheeraphonpDEV@users.noreply.github.com>
2022-07-20 12:47:46 +0700

    process - 5
    UI updated
    1. Product
    2. Home page
    3. Navigator
    4. Side Bar

--- clothnix/framework/common/types/product.ts
+++ clothnix/framework/common/types/product.ts
@@ -1,13 +1,20 @@
+
 export interface ProductImage {
-    url: string
-    alt?: string
-  }
-  
-  export interface Product {
-    id: string
-    name: string
-    description: string
-    slug: string
-    path: string
-    images: ProductImage[]
-  }
+  url: string
+  alt?: string
+}
+
+export interface ProductPrice {
+  value: number
+  currencyCode: "USD" | "EUR" | string
+}
+
+export interface Product {
+  id: string
+  name: string
+  description: string
+  slug: string
+  path: string
+  images: ProductImage[]
+  price: ProductPrice
+}
