0f43de5e749f0f5ff0019b33137189a581581301
TPGitz <103758766+TheeraphonpDEV@users.noreply.github.com>
2022-07-22 13:17:50 +0700

    process - 12
    1. update SWRHook for cart.
    2. create UI on cart side to add items from add to card.
    3.create delete item  function on cart.

--- clothnix/framework/shopify/utils/mutations/index.ts
+++ clothnix/framework/shopify/utils/mutations/index.ts
@@ -1,2 +1,4 @@
 export { default as checkoutLineItemsAddMutation } from "./checkout-line-items-add"
 export { default as checkoutCreateMutation } from "./checkout-create"
+export { default as checkoutLineItemsRemoveMutation } from "./checkout-line-items-remove"
+
